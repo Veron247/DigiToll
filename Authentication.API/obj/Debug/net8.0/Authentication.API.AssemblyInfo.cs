35df2872b2150d9ab86b12e297ab16a35d54f5c3
NtiriBoateng <ntiriboateng.nb@gmail.com>
2025-03-18 20:26:36 +0000

    authentication completed

--- Authentication.API/obj/Debug/net8.0/Authentication.API.AssemblyInfo.cs
+++ Authentication.API/obj/Debug/net8.0/Authentication.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e8b7ef83d61582e48199b1d73662db677f5f2df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21eba0cb2750fbf62ce14042dc54e2daf257145")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Authentication.Services/obj/Debug/net8.0/Authentication.Services.AssemblyInfo.cs
+++ Authentication.Services/obj/Debug/net8.0/Authentication.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e8b7ef83d61582e48199b1d73662db677f5f2df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21eba0cb2750fbf62ce14042dc54e2daf257145")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
